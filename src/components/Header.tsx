66f272ca3bb2c3f791d212b7cd47509007810895
Nikita K <rabot422@gmail.com>
2024-01-15 23:51:58 +0300

    implement link to the store route

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 export const Header = () => {
+  const HeaderWrap = styled.header`
+  height:120px;
+  font-size: 1.5em;
+  text-align: center;
+  color: #BF4F74;
+  background-color:grey;
+`;
   return (
-    <div>Header</div>
+    <HeaderWrap>
+      <Link to='store'>store</Link>
+    </HeaderWrap>
   )
 }
--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -1,6 +1,3 @@
-import React from 'react'
-import { Cart } from './Cart'
-import { Store } from './Store'
 
 export const HomePage = () => {
   return (
