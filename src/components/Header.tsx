f6c23a4d0a2be315953df89a3542b6eb1016380c
Nikita K <rabot422@gmail.com>
2024-01-16 00:03:01 +0300

    style header a bit

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,15 +4,30 @@ import styled from 'styled-components';
 
 export const Header = () => {
   const HeaderWrap = styled.header`
-  height:120px;
+  padding:0 20px;
+  height:80px;
+  display:flex;
+  justify-content:space-between;
+  align-items:center;
   font-size: 1.5em;
   text-align: center;
   color: #BF4F74;
   background-color:grey;
 `;
+const Logo = styled.a`
+  font-size:2rem;
+  font-weight:bold;
+  color: #fdfdfd;
+`
   return (
     <HeaderWrap>
-      <Link to='store'>store</Link>
+      <Logo>LOGOSHOP</Logo>
+      <input type="text" />
+      <nav>
+        <Link to='/'>Home</Link>
+        <Link to='store'>Store</Link>
+      </nav>
+     
     </HeaderWrap>
   )
 }
