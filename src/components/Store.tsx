befb207ca01c81b24f03eface5a916e5fa3a6559
Nikita K <rabot422@gmail.com>
2024-01-30 20:51:07 +0300

    finish style store page

--- src/components/Store.tsx
+++ src/components/Store.tsx
@@ -3,6 +3,8 @@ import React, { useEffect, useState } from 'react';
 import { CartItem, Item } from './Router';
 import styled from 'styled-components';
 
+import ADD_TO_CART from '../assets/icons/add-to-cart.png'
+
 export const ProductList = styled.ul`
   display: flex;
   flex-wrap: wrap;
@@ -17,23 +19,63 @@ const Loading = styled.div`
   margin: 200px 0;
 `;
 const ProductItem = styled.li`
-  height: 400px;
-  width: 400px;
-  border:2px solid grey;
-  border-radius:8px;
+  height: 440px;
+  width: 440px;
+  border:2px solid #010101;
+  border-radius:14px;
   display:grid;
   justify-items:center;
   list-style-type: none;
+  transition: all 0.2s;
+  &:hover{
+    box-shadow: 0 0 7px black;
+  }
   img {
     height: 250px;
+    margin-top: 5px;
   }
   div{
     background-color: #f3f3f3;
     width:100%;
+    border-radius: 0 0 14px 14px;
+    display:grid;
+    grid-template-columns:100px 1fr 80px;
+    align-items:center;
+  }
+  span{
+    display:block;
+    font-size:1.3rem;
+    margin:10px;
+    grid-column:1/4
+  }
+  p{
+    font-size: 2.3rem;
+    font-weight: bold;
+    text-align:center;
+    margin:10px;
   }
 `
 const StoreWrap = styled.section`
     margin:45px;
+`
+const AddCartBtn = styled.button`
+  padding:7px;
+  border-radius: 14px;
+  grid-column:3/4;
+  margin:10px;
+  cursor:pointer;
+  border:none;
+  background-color: #dedede;
+  &:hover{
+    background-color: #c7c7c7;
+  }
+  &:active{
+    background-color: #9dffb0;
+  }
+img{
+  pointer-events: none;
+  height:40px;
+}
 `
 interface StoreProps {
   items: Item[];
@@ -61,8 +103,9 @@ export const Store = ({
   }, []);
 
   const addToCart = (event: React.MouseEvent) => {
+    // event.stopPropagation()
     const target = event.target as HTMLButtonElement;
-    const currentId: string | undefined = target.parentElement?.id;
+    const currentId: string | undefined = target.closest('button').parentElement?.parentElement?.id;
     const filteredElem: Item = items.filter((item: Item) => item.id === Number(currentId))[0];
 
     const tempArr = [...cartItems];
@@ -95,9 +138,11 @@ export const Store = ({
               <ProductItem key={item.id} id={item.id.toString()}>
                 <img src={item.image} alt="" />
                 <div>
-                  <p>{item.title}</p>
-                <p>${item.price}</p>
-                <button onClick={(e) => addToCart(e)}>Add to Cart</button>
+                  <span>{item.title}</span>
+                  <p>${item.price}</p>
+                  <AddCartBtn onClick={(e) => addToCart(e)}>
+                    <img  src={ADD_TO_CART} alt="Add to cart" />
+                  </AddCartBtn>
                 </div>
                 
               </ProductItem>
