91407030ac5223373102972f3630a3a816596a34
Nikita K <rabot422@gmail.com>
2024-02-04 01:42:29 +0300

    create working test

--- App.test.tsx
+++ App.test.tsx
@@ -1,14 +1,29 @@
+import React from 'react'
 import { describe, it, expect } from 'vitest';
 import { render, screen } from "@testing-library/react";
-import App from "./src/App";
+import {ErrorPage} from "./src/components/ErrorPage";
+import '@testing-library/jest-dom';
+import { BrowserRouter } from 'react-router-dom';
 
+describe("ErrorPage component", () => {
+    it("renders correct ", () => {
+      render( <BrowserRouter>
+      <ErrorPage />
+      </BrowserRouter>
+      
+      );
+      const link = screen.getByText("Oh no, this route doesn't exist!");
 
-describe("App component", () => {
-    it("renders correct heading", () => {
-      render(<App />);
-      expect(screen.getByRole("heading").textContent).toMatch(/our first test/i);
+        expect(link).toBeInTheDocument();
     });
-  });
+
+    it("fake test ", () => {
+      expect(5).toBe(5)
+  })
+  }
+
+
+  )
   
 
 
